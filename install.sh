4005302a4f418ebb8031396005dc42ef889f6b07
stefanableitinger <stefanableitinger@hotmail.com>
2021-03-24 21:34:07 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -94,7 +94,7 @@ then
     unset ZSH
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions

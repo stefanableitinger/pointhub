f616631e63e26c1ae2df294c684f98c6b81d7130
stefanableitinger <stefanableitinger@hotmail.com>
2021-09-27 16:56:04 +0200

    modified:   fm4.sh

--- fm4.sh
+++ fm4.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-mplayer http://ors-sn04.ors-shoutcast.at/fm4-q1a
+if pidof -x mplayer > /dev/null
+then
+  killall mplayer
+else
+  mplayer http://ors-sn04.ors-shoutcast.at/fm4-q1a </dev/null >/dev/null 2>&1 &
+  #cava
+fi
+

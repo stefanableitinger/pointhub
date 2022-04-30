1c0995eb3a09aec3064bcde2eb87eafd788f8741
k <k@localhost.localdomain>
2022-04-30 10:43:55 +0200

    Changes to be committed:

--- setup.sh
+++ setup.sh
@@ -35,14 +35,23 @@ echo "exec i3" >> /home/k/.xinitrc
 #arch tools
 sudo pacman -Sy xorg xorg-xinit base-devel rofi i3-gaps --noconfirm
 
-#void tools
+#config (--l=25)
+curl -so .config/i3/config https://raw.githubusercontent.com/stefanableitinger/pointhub/master/config
+curl -so .config/conky.conf https://raw.githubusercontent.com/stefanableitinger/pointhub/master/conky.conf
+curl -so .config/picom.conf https://raw.githubusercontent.com/stefanableitinger/pointhub/master/picom.conf
+curl -O https://raw.githubusercontent.com/stefanableitinger/pointhub/master/.xinitrc
+curl -O https://raw.githubusercontent.com/stefanableitinger/pointhub/master/.tmux.conf
+curl -O https://raw.githubusercontent.com/stefanableitinger/pointhub/master/.bashrc
+
+#void tools (--l=17)
 sudo xbps-install -y neovim tmux curl xorg i3-gaps rofi chromium picom conky nitrogen base-devel fontconfig-devel libX11-devel libXft-devel bsdtar git
 
-#font
+#font (--l=14)
 mkdir -p /home/k/.local/share/fonts/spacemono-nf
 curl -so /home/k/.local/share/fonts/spacemono-nf/Space\ Mono\ Nerd\ Font\ Complete\ Mono.ttf https://raw.githubusercontent.com/stefanableitinger/pointhub/master/spacemono-nf.ttf
 fc-cache -rf /home/k/.local/share/fonts/spacemono-nf
-#st
+
+#st (--l=9)
 mkdir -p /home/k/Downloads
 curl -s -O --output-dir /home/k/Downloads/ https://dl.suckless.org/st/st-0.8.4.tar.gz
 bsdtar -xpf /home/k/Downloads/st-0.8.4.tar.gz -C /home/k/Downloads/
@@ -51,7 +60,3 @@ cd /home/k/Downloads/st-0.8.4
 patch -p1 < st-scrollback-0.8.4.diff
 sed -i 's/Liberation Mono:pixelsize=12/SpaceMono Nerd Font Mono:pixelsize=20/g' config.def.h
 sudo make clean install
-#config
-curl -so .config/i3/config https://raw.githubusercontent.com/stefanableitinger/pointhub/master/config
-curl -O https://raw.githubusercontent.com/stefanableitinger/pointhub/master/.tmux.conf
-curl -O https://raw.githubusercontent.com/stefanableitinger/pointhub/master/.bashrc

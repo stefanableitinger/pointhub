96fd8ce719b448ce5ea46716da130994f666e03b
stefanableitinger <56166006+stefanableitinger@users.noreply.github.com>
2024-05-26 19:48:53 +0200

    Add files via upload

--- mps.sh
+++ mps.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 search_result_videos=()
 video_cue=()
+player_windows=()
 
-mpv_command_parameter="--mute=yes --start=96% --end=99% --auto-window-resize=no --loop-playlist=inf --keepaspect-window=no"
+mpv_command_parameter="--pause --mute=yes --start=96% --end=99% --auto-window-resize=no --loop-playlist=inf --keepaspect-window=no"
 search_path="/home/k/Videos/xyz/"
 search_pattern="*"
 number_of_players=4
@@ -15,11 +16,29 @@ floating="True"
 IFS="
 "
 
-# get base directory for videos from 3rd parameter
-if ! [ "$3" == "" ]; then search_path="$3"; fi
 
-# get search string for videos from 2nd parameter
-if ! [ "$2" == "" ]; then search_pattern="*$2*"; fi
+if [ "$#" -eq 3 ]; then
+	search_pattern="*$2*"
+	search_path="$3"
+elif [ "$#" -eq 2 ]; then
+	search_path="$2"
+elif [ "$#" -eq 1 ]; then
+	if [ "$1" == "--help" ] || [ "$1" == "--h" ] || [ "$1" == "-help" ] || [ "$1" == "-h" ]; then
+		echo "usage: mps"
+		echo "1st parameter is the number of windows (if prefixed 'x' eg x1 then without floating mode)" 
+		echo " 	if two parameters are provided"
+		echo "		2nd parameter is path to search for videos"
+		echo " 	if three parameters are provided"
+		echo "		2nd parameter is search string"
+		echo "		3rd parameter is path to search for videos that contain the search string"
+		echo ""
+		echo "without parameters defaults are used"
+		echo "	number of floating windows: $number_of_players"
+		echo "	search path: $search_path"
+		echo "	search pattern: $search_pattern"
+		exit
+	fi
+fi
 
 # create list of video files in random order
 mapfile -t videos < <(find "$search_path" -type f -iname "$search_pattern" -a \( -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.webm" \) )
@@ -31,7 +50,7 @@ if [ "$DISPLAY" = "" ]; then
 fi
 
 # get number of players on screen from 1st parameter prefix x and windows are not floating
-if ! [ "$1" = "" ]; then 
+if [ ! "$1" = "" ]; then 
 	if [[ "$1" == *"x"* ]]; then floating="False"; fi
 	number_of_players=$(echo "$1" | cut -dx -f2)
 fi
@@ -67,10 +86,11 @@ for player in $(seq 1 "$number_of_players"); do
 	bash -c "mpv $mpv_command_parameter ${player_cue[$player_index]} --input-ipc-server=/tmp/mps_player_index_$player_guid < /dev/null > /dev/null 2>&1 &"
 	player_pid=$(ps wwwaux | grep -i "[m]pv.*mps_player_index_$player_guid" | awk '{print $2}')
 	player_window=$(xdotool search --sync --pid "$player_pid")
+	player_windows+=("$player_window")
 
 	case $number_of_players in
 		1)
-			window_width=$(printf "%.0f" "$(echo "$screen_width - 20" | bc -l)")
+window_width=$(printf "%.0f" "$(echo "$screen_width - 20" | bc -l)")
 			window_height=$(printf "%.0f" "$(echo "$screen_height - 40" | bc -l)")
 			posx=10
 			posy=30;;
@@ -114,4 +134,8 @@ for player in $(seq 1 "$number_of_players"); do
 	if [ "$floating" == "True" ]; then 
 		i3-msg "[ id=$player_window ] floating enable, resize set $window_width px $window_height px, move position $posx px $posy px"
 	fi
+done
+
+for player_window in "${player_windows[@]}"; do
+	xdotool key --window "$player_window" space
 done
